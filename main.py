e39d8292f57c3210856a3411256202f842abc816
ken-wilderotter <sophia_wilderotter@biswashington.org>
2025-01-23 15:12:01 +0000

    í

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+with open("students.txt", "r") as file:
+    for line in file:
+        print (line.strip())
 
+
+with open("students.txt", "r") as file:
+     for line in file:
+          print(line[1])
+
+
+with open("students.txt", "r") as file:
+     for line in file:
+          if len > 6: 
+            print
+
+
+new-line = input("Enter a name to add to the list")
+with open("students.txt", "a") as file:
+    file.write(new-line + "/n")
